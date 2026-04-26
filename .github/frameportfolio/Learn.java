69565f8c3159d3c196ef0f66df0334f9d3a7a691
chaakemadile <chaakemadile24@gmail.com>
2026-04-26 22:09:47 +0200

    Change of positions

---
+++ .github/frameportfolio/Learn.java
@@ -0,0 +1,14 @@
+public class Learner {
+    public void forget() {
+        // Implementation of forget method
+    }
+}
+
+public class Learn {
+    public static void main(String[] args) {
+        //Create a learner object
+        Learner learner = new Learner();
+    // Replace 'for' with the correct method, e.g., 'forget()'
+    learner.forget();
+    }
+} 
